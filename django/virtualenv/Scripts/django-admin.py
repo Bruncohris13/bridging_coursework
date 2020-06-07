04c8ef3cda928da548e7fad552bdad9cb743e196
Christos Efstathiou <cefstathiou13@gmail.com>
2020-06-07 15:32:01 +0300

    Created virtualvenv for Windows

---
+++ django/virtualenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\bruno\onedrive - university of birmingham\modules\year 2\bridging coursework\bridging_coursework\django\virtualenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
