d2177c6176f1c75ad3a49b9f70fc698528a5e3c5
Christos Efstathiou <cefstathiou13@gmail.com>
2020-06-10 16:26:08 +0300

    Made small correction in functional_tests.py

--- django/functional_tests.py
+++ django/functional_tests.py
@@ -9,11 +9,10 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):  
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):  
-        self.browser.get('http://localhost:8000/blog')
+    def test_start_page(self):  
+        self.browser.get('http://localhost:8000')
 
-        self.assertIn('Efstathiou', self.browser.title)  
-        self.fail('Finish the test!')  
+        self.assertIn('Christos Efstathiou', self.browser.title)
 
 if __name__ == '__main__':  
     unittest.main(warnings='ignore')  
