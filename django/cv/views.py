324eba976f51698c83ec1c1ec10551744a489658
Christos Efstathiou <cefstathiou13@gmail.com>
2020-06-11 15:07:45 +0300

    Created cv folder in static and templates

--- django/cv/views.py
+++ django/cv/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home_page.html', {})
+    return render(request, 'cv/home_page.html', {})
