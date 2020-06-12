a65ca7da3905f7f9aed4cccf068053aab8ef8869
Christos Efstathiou <cefstathiou13@gmail.com>
2020-06-12 15:08:46 +0300

    Created bio_edit.html

--- django/cv/views.py
+++ django/cv/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import BioForm
+from .models import Bio
 
 # Create your views here.
-def home_page(request):
+def bio_edit(request):
     if request.method == 'POST':
         form = BioForm(request.POST, request.FILES)
         if form.is_valid():
@@ -10,6 +11,12 @@ def home_page(request):
             return redirect('home_page')
     else:
         form = BioForm()
-    return render(request, 'cv/home_page.html', {
+    return render(request, 'cv/bio_edit.html', {
         'form': form
+    })
+
+def home_page(request):
+    bio = Bio.objects.all()
+    return render(request, 'cv/home_page.html', {
+        'bio': bio
     })
