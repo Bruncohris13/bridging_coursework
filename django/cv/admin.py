50815bf01a74107feebe1c0f3185d8c636cab978
Christos Efstathiou <cefstathiou13@gmail.com>
2020-06-21 15:53:58 +0300

    Added models to admin

--- django/cv/admin.py
+++ django/cv/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(CvPdf)
+admin.site.register(Bio)
+admin.site.register(CVPost)
+admin.site.register(EducationPost)
+admin.site.register(WorkPost)
+admin.site.register(AchievementPost)
+admin.site.register(QualificationPost)
+admin.site.register(SkillPost)
+admin.site.register(InterestPost)
+admin.site.register(ProjectPost)
