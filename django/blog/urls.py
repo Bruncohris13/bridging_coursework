101811c18f595eb226bee70f34cc5d021d8ca52b
Christos Efstathiou <cefstathiou13@gmail.com>
2020-07-02 16:21:17 +0300

    Added Edit Button for Blog Comments

--- django/blog/urls.py
+++ django/blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
-    path('post/<int:pk>/delete_comment/<int:cm>/', views.comment_delete, name='comment_delete'),
+    path('post/<int:pk>/comment_edit/<int:cm>/', views.comment_edit, name='comment_edit'),
+    path('post/<int:pk>/comment_delete/<int:cm>/', views.comment_delete, name='comment_delete'),
 ]
