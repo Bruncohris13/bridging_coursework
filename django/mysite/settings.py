d3d4b8686a4603cd035cb67ec8522bf287d02713
Christos Efstathiou <cefstathiou13@gmail.com>
2024-02-06 15:52:49 +0000

    Changed to DEBUG = False

--- django/mysite/settings.py
+++ django/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$@y#7d*8h1*ai$p_30d1*(cpu#k#p=j+btd4134yzsahjcr^$s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["brunochris13.eu.pythonanywhere.com", "127.0.0.1", "localhost"]
 
